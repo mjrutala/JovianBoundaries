85401a77e1f6a301dc63ebdaf83f688bc3509dac
mjrutala <mrutala@bu.edu>
2024-06-07 13:40:36 +0100

    Added analyze_RhoDistributions() to look at cylindrical rho vs pressure for different dawn-dusk planes down the tail and compare to Juno orbit to find over/under densities

--- code/compare_BoundariesToData.py
+++ code/compare_BoundariesToData.py
@@ -15,6 +15,7 @@ from scipy.optimize import curve_fit
 from scipy import odr
 import pandas as pd
 
+from scipy.optimize import least_squares
 
 import sys
 import JoyBoundaryCoords as JBC
@@ -23,6 +24,7 @@ sys.path.append('/Users/mrutala/projects/MMESH/mmesh/')
 import MMESH_reader
 
 import boundaries
+import JunoPreprocessingRoutines as JPR 
 
 plt.style.use('/Users/mrutala/code/python/mjr.mplstyle')
 
@@ -380,6 +382,148 @@ def compare_Pressures():
     plt.show()
 
 
+def analyze_RhoDistributions():
+    import spiceypy as spice
+    import numpy as np
+    import matplotlib.pyplot as plt
+    
+    crossings_df = JPR.make_CombinedCrossingsList(boundary = 'MP')
+    crossings_df = crossings_df.drop(['p_dyn', 'r_mp', 'r_bs'], axis='columns')
+    
+    rpl_crossings = boundaries.convert_CartesianToCylindricalSolar(*crossings_df.loc[:, ['x_JSS', 'y_JSS', 'z_JSS']].to_numpy().T)
+    crossings_df[['rho', 'phi', 'ell']] = rpl_crossings.T
+    
+    #   Load the solar wind data, select the ensemble, and add it to the df
+    sw_models = MMESH_reader.fromFile('/Users/mrutala/projects/JupiterBoundaries/mmesh_run/MMESH_atJupiter_20160301-20240301_withConstituentModels.csv')
+    sw_mme = sw_models.xs('ensemble', axis='columns', level=0)
+    hourly_crossings = JPR.get_HourlyFromDatetimes(crossings_df.index)
+    crossings_df.loc[:, ['p_dyn_mmesh', 'p_dyn_nu_mmesh', 'p_dyn_pu_mmesh']] = sw_mme.loc[hourly_crossings, ['p_dyn', 'p_dyn_neg_unc', 'p_dyn_pos_unc']].to_numpy()
+    
+    #   Load SPICE kernels for positions of Juno
+    spice.furnsh(JPR.get_paths()['PlanetaryMetakernel'].as_posix())
+    spice.furnsh(JPR.get_paths()['JunoMetakernel'].as_posix())
+    
+    R_J = spice.bodvcd(599, 'RADII', 3)[1][1]
+    
+    #   Get all hourly trajectory info
+    earliest_time = min(crossings_df.index)
+    latest_time = max(crossings_df.index)
+     
+    datetimes = np.arange(pd.Timestamp(earliest_time).replace(minute=0, second=0, nanosecond=0),
+                          pd.Timestamp(latest_time).replace(minute=0, second=0, nanosecond=0) + dt.timedelta(hours=1),
+                          dt.timedelta(hours=1)).astype(dt.datetime)
+     
+    ets = spice.datetime2et(datetimes)   
+    
+    pos, lt = spice.spkpos('Juno', ets, 'Juno_JSS', 'None', 'Jupiter')
+    xyz = pos.T / R_J   
+    
+    spice.kclear()
+    
+    rpl_Juno = boundaries.convert_CartesianToCylindricalSolar(*xyz)
+    Juno_df = pd.DataFrame(data = rpl_Juno.T, columns=['rho', 'phi', 'ell'], index = datetimes)
+    
+    #   Format the dataframe
+    
+    #   Boundaries in the elevation (z cylindrical axis + toward Sun)
+    ell_bounds = np.array([np.linspace(-20, -120, 6), np.linspace(0, -100, 6)]).T
+    
+    for ell_bound in ell_bounds:
+        
+        #   Binary truth indices for bounds, dawn/dusk
+        #   In this coordinate system, dawn is 0-pi and dusk is pi-2pi
+        crossings_dawn_query = '({0} <= ell < {1}) & (phi > 0)'.format(*ell_bound)
+        crossings_dusk_query = '({0} <= ell < {1}) & (phi < 0)'.format(*ell_bound)
+        
+        rho_bin_edges = np.arange(50, 130, 5)
+        p_dyn_bin_edges = np.logspace(-2.5, -0.5, 21)
+        
+        #   Plot rho vs p_dyn for dawn and dusk hemisphere
+        fig, axd = plt.subplot_mosaic([['dawn_scatter', 'dawn_p_dyn', '.', 'dusk_scatter', 'dusk_p_dyn'],
+                                       ['dawn_rho', '.', '.', 'dusk_rho', '.']], 
+                                        width_ratios = [3, 1, 0.5, 3, 1], height_ratios=[3, 1],
+                                        figsize=[6,3])
+        plt.subplots_adjust(bottom=0.1, left=0.1, top=0.95, right=0.95, 
+                            hspace=0, wspace=0)
+        
+        def errorscatter_rho_p_dyn(ax, df):
+            ax.errorbar(df['rho'],
+                        df['p_dyn_mmesh'],
+                        yerr = df[['p_dyn_nu_mmesh', 'p_dyn_pu_mmesh']].to_numpy().T,
+                        linestyle = 'None', marker = 'o', markersize = 4,
+                        elinewidth = 1.0)   
+        
+        errorscatter_rho_p_dyn(axd['dawn_scatter'], crossings_df.query(crossings_dawn_query))
+        errorscatter_rho_p_dyn(axd['dusk_scatter'], crossings_df.query(crossings_dusk_query))
+        
+        def histogram_rho(ax, df, **kwargs):
+            histo, _ = np.histogram(df['rho'], 
+                                    bins=rho_bin_edges,
+                                    density=True)
+            ax.stairs(histo, rho_bin_edges, 
+                      linewidth=2, **kwargs)
+            
+        histogram_rho(axd['dawn_rho'], crossings_df.query(crossings_dawn_query), color='C1')
+        histogram_rho(axd['dawn_rho'], Juno_df.query(crossings_dawn_query), color='C2')
+        
+        histogram_rho(axd['dusk_rho'], crossings_df.query(crossings_dusk_query), color='C1')
+        histogram_rho(axd['dusk_rho'], Juno_df.query(crossings_dusk_query), color='C2')
+        
+        def histogram_p_dyn(ax, df):
+            histo, _ = np.histogram(df['p_dyn_mmesh'],
+                                    bins = p_dyn_bin_edges,
+                                    density=True)
+            ax.stairs(histo, p_dyn_bin_edges,
+                      linewidth=2, color='C3',
+                      orientation='horizontal')
+            
+        histogram_p_dyn(axd['dawn_p_dyn'], crossings_df.query(crossings_dawn_query))
+        histogram_p_dyn(axd['dusk_p_dyn'], crossings_df.query(crossings_dusk_query))
+        
+        for ax_name in ['dawn_scatter', 'dawn_rho', 'dusk_scatter', 'dusk_rho']:
+            axd[ax_name].set(xlim = (rho_bin_edges[0], rho_bin_edges[-1]))
+        for ax_name in ['dawn_scatter', 'dawn_p_dyn', 'dusk_scatter', 'dusk_p_dyn']:
+            axd[ax_name].set(ylim = (p_dyn_bin_edges[0], p_dyn_bin_edges[-1]), 
+                             yscale = 'log')
+        for ax_name in ['dawn_p_dyn', 'dusk_p_dyn']:
+            axd[ax_name].set(yticklabels='')
+            
+        plt.show()
+        #breakpoint()
+        # hist_crossings, bin_edges = np.histogram(crossings_dawn_query.loc[:, ['rho']], 
+        #                                          density=True, 
+        #                                          bins=rho_bin_edges)
+        # # hist_Juno, _ = np.histogram(rpl_Juno[0, indx_Juno],
+        # #                          density=True, 
+        # #                          bins=np.arange(30, 140, 10))
+        
+        # # hist_joint = hist_crossings/hist_Juno
+        
+        
+        
+        # fig, axs = plt.subplots(nrows=2, sharex=True)
+        # axs[0].stairs(hist_crossings, bin_edges, linewidth=4, label='Crossings')
+        # #axs[0].stairs(hist_Juno, bin_edges, linewidth=4, label='Juno')
+        # axs[0].legend()
+        
+        # #axs[1].stairs(hist_joint, bin_edges)
+        
+        # plt.show()
+        
+        
+        
+        # # res = np.histogram2d(rpl_crossings[0, indx_crossings], sw_mme_sample['p_dyn'],
+        # #                      [np.arange(30, 140, 10), np.arange(0.01, 0.11, 0.01)])
+        
+        # # plt.imshow(res[0], origin='lower', extent=[30, 130, 0.01, 0.1], aspect=1000)
+        # # plt.show()
+        
+        # plt.scatter(rpl_crossings[0, indx_crossings], sw_mme_sample['p_dyn'])
+        # plt.show()
+    
+    breakpoint()
+        
+
 def runner(parameter_distributions=False):
     
     which_boundary = 'MP'
@@ -399,7 +543,7 @@ def runner(parameter_distributions=False):
         crossing_data = pd.concat([bs_crossing_data_Louis2023, bs_crossing_data_Kurth], axis=0, join="outer")
     
     #   Only pass the datetime index, the direction of the crossing, and any notes
-    hourly_crossing_data = PR.make_HourlyCrossingList(crossing_data.loc[:, ['direction', 'notes']])
+    hourly_crossing_data = PR.make_HourlyCrossingList(crossing_data)
     
     #   Load the solar wind data and select the ensemble
     sw_models = MMESH_reader.fromFile(sw_mme_filepath)
@@ -408,7 +552,10 @@ def runner(parameter_distributions=False):
     #   Select boundary model
     boundary_models = {'Shuelike': {'model': boundaries.Shuelike, 
                                     'params': ['r0', 'r1', 'a0', 'a1'], 
-                                    'guess': [60, -0.15, 0.5, 1.0]},
+                                    'guess': [60, -0.155, 0.5, -0.1],
+                                    'bounds': ([0, -0.16, 0.2, -1.0],
+                                               [200, -0.15, 0.8, 1.0])
+                                    },
                        'Shuelike_UniformPressureExponent': {'model': boundaries.Shuelike_UniformPressureExponent, 
                                                        'params': ['r0', 'r1', 'a0', 'a1'], 
                                                        'guess': [60, -0.15, 0.5, 1.0]},
@@ -416,8 +563,11 @@ def runner(parameter_distributions=False):
                                                        'params': ['r0', 'r1', 'a0', 'a1', 'a2'], 
                                                        'guess': [60, -0.15, 0.5, 1.0, 1.0]},
                        'Shuelike_AsymmetryCase1': {'model': boundaries.Shuelike_AsymmetryCase1, 
-                                                       'params': ['r0', 'r1', 'r2', 'r3', 'a0', 'a1'], 
-                                                       'guess': [60, -0.15, 5, 5, 0.5, 1.0]},
+                                                       'params': ['r0', 'r1', 'r2', 'r3', 'r4', 'a0', 'a1'], 
+                                                       'guess': [50, -0.15, 5, 5, 0, 0.5, 0.0],
+                                                       'bounds': ([30, -0.3, 0, 0, -3.14, 0.3, -1.0],
+                                                                  [80, -0.1, 10, 10, 3.14, 0.8, 1.0])
+                                                       },
                        'Shuelike_Asymmetric': {'model': boundaries.Shuelike_Asymmetric, 
                                                        'params': ['r0', 'r1', 'r2', 'a0', 'a1', 'a2'], 
                                                        'guess': [60, -0.15, 1, 0.5, 1.0, 1.0]},
@@ -450,7 +600,15 @@ def runner(parameter_distributions=False):
         p_p_dyn = perturb_Placeholder(*sw_mme.loc[:, ['p_dyn', 'p_dyn_neg_unc', 'p_dyn_pos_unc']].to_numpy().T)
         perturbed_sw_mme = pd.DataFrame({'p_dyn': p_p_dyn}, index=sw_mme.index)
         
-        res = fit_Boundary_withODR(perturbed_hcd, perturbed_sw_mme, boundary_model['model'], boundary_model['guess'])
+        #   Resampling of data
+        # rng = np.random.default_rng()
+        # bootstrap_indx = np.sort(rng.integers(0, len(perturbed_hcd)-1, len(perturbed_hcd)))
+        # breakpoint()
+        # perturbed_hcd = perturbed_hcd.iloc[bootstrap_indx, :]
+        
+        res = fit_Boundary_withLS(perturbed_hcd, perturbed_sw_mme, boundary_model['model'], boundary_model['guess'], boundary_model['bounds'])
+        
+        # res = fit_Boundary_withODR(perturbed_hcd, perturbed_sw_mme, boundary_model['model'], boundary_model['guess'])
         
         stats['parameters'].append(res['parameters'])
         
@@ -639,7 +797,7 @@ def runner(parameter_distributions=False):
     axd['3d'].view_init(elev=30, azim=225, roll=0)
     axd['3d'].set(xlabel = r'$x_{JSS}/R_J$ (+ sunward)', xlim = (100, -150), 
                   ylabel = r'$y_{JSS}/R_J$ (+ duskward)', ylim = (125, -125), 
-                  zlabel = r'$z_{JSS}/R_J$ (+ northward)', zlim = (-125, 125))
+                  zlabel = r'$z_{JSS}/R_J$ (+ northward)', zlim = (0, 250))
     
     clip_indx = ((z_grid < 0) | (x_grid < -150))
     x_grid[clip_indx] = np.nan
@@ -746,7 +904,67 @@ def perturb_Placeholder(variable, neg_unc, pos_unc):
     
     return perturbed_variable
 
-def fit_Boundary(crossings, solarwind_model, function, initial_parameters):
+def fit_Boundary_withLS(crossings, solarwind_model, function, initial_parameters, bounds=None):
+    """
+    Generally more flexible than ODR as we can supply a loss function, scales, 
+    bounds on parameters, and more.
+    
+    Since we perform custom uncertainty analysis with perturbations, we don't
+    need the uncertainty-handling of ODR
+
+    Parameters
+    ----------
+    crossings : TYPE
+        DESCRIPTION.
+    solarwind_model : TYPE
+        DESCRIPTION.
+    function : TYPE
+        DESCRIPTION.
+    initial_parameters : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    TYPE
+        DESCRIPTION.
+
+    """
+    
+    #   Add the model parameters as variables, 
+    #   then take only times with both model and crossing
+    variables = pd.concat([crossings, solarwind_model], axis=1, join="inner")
+    
+    #   Fetch the expected variables from the function being fit
+    independent_variables, dependent_variables = function(variables=True)
+    
+    independents = variables.loc[:, independent_variables].to_numpy().T
+    dependents = variables.loc[:, dependent_variables].to_numpy().T
+    
+    def residuals(parameters, independents, dependents):
+        rhs = function(parameters=parameters, coordinates=independents)
+        zero = rhs - dependents
+        return zero
+    
+    if bounds is None:
+        bounds = ([-np.inf] * len(initial_parameters),
+                  [np.inf] * len(initial_parameters))
+    
+    res_lsq = least_squares(residuals, initial_parameters, 
+                            args=(independents, dependents),
+                            bounds = bounds,
+                            x_scale = 'jac',
+                            loss='linear')
+    
+    #   Get errors
+    estimated_dependents = function(res_lsq.x, independents)
+    rmsd = np.sqrt((1/len(dependents)) * np.sum((dependents - estimated_dependents)**2))
+    mae = np.mean(np.abs(dependents - estimated_dependents))
+    
+    result = {'parameters': res_lsq.x,
+              'rmsd': rmsd,
+              'mae': mae}
+
+    return result
     
     #   Add the model parameters as variables, 
     #   then take only times with both model and crossing
@@ -771,6 +989,9 @@ def fit_Boundary(crossings, solarwind_model, function, initial_parameters):
     parameters = odr_result.beta, odr_result.sd_beta
     
     return parameters
+
+
+
 
 def fit_Boundary_withODR(crossings, solarwind_model, function, initial_parameters):
     
