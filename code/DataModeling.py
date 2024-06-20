2dbd843c3f07dc0641a49665759e88cb364bbbf1
mjrutala <mrutala@bu.edu>
2024-06-20 09:57:06 +0100

    Added import calls, removed defunct code

--- code/DataModeling.py
+++ code/DataModeling.py
@@ -5,6 +5,16 @@ Created on Thu Jun 20 09:48:26 2024
 
 @author: mrutala
 """
+import numpy as np
+import sys
+import datetime
+import pandas as pd
+
+import BoundaryModels as BM
+import JunoPreprocessingRoutines as JPR
+
+sys.path.append('/Users/mrutala/projects/MMESH/mmesh/')
+import MMESH_reader
 
 def analyze_RhoDistributions():
     import spiceypy as spice
@@ -14,7 +24,7 @@ def analyze_RhoDistributions():
     crossings_df = JPR.make_CombinedCrossingsList(boundary = 'MP')
     crossings_df = crossings_df.drop(['p_dyn', 'r_mp', 'r_bs'], axis='columns')
     
-    rpl_crossings = boundaries.convert_CartesianToCylindricalSolar(*crossings_df.loc[:, ['x_JSS', 'y_JSS', 'z_JSS']].to_numpy().T)
+    rpl_crossings = BM.convert_CartesianToCylindricalSolar(*crossings_df.loc[:, ['x_JSS', 'y_JSS', 'z_JSS']].to_numpy().T)
     crossings_df[['rho', 'phi', 'ell']] = rpl_crossings.T
     
     #   Load the solar wind data, select the ensemble, and add it to the df
@@ -34,8 +44,8 @@ def analyze_RhoDistributions():
     latest_time = max(crossings_df.index)
      
     datetimes = np.arange(pd.Timestamp(earliest_time).replace(minute=0, second=0, nanosecond=0),
-                          pd.Timestamp(latest_time).replace(minute=0, second=0, nanosecond=0) + dt.timedelta(hours=1),
-                          dt.timedelta(hours=1)).astype(dt.datetime)
+                          pd.Timestamp(latest_time).replace(minute=0, second=0, nanosecond=0) + datetime.timedelta(hours=1),
+                          datetime.timedelta(hours=1)).astype(datetime.datetime)
      
     ets = spice.datetime2et(datetimes)   
     
@@ -44,7 +54,7 @@ def analyze_RhoDistributions():
     
     spice.kclear()
     
-    rpl_Juno = boundaries.convert_CartesianToCylindricalSolar(*xyz)
+    rpl_Juno = BM.convert_CartesianToCylindricalSolar(*xyz)
     Juno_df = pd.DataFrame(data = rpl_Juno.T, columns=['rho', 'phi', 'ell'], index = datetimes)
     
     #   Format the dataframe
@@ -113,36 +123,5 @@ def analyze_RhoDistributions():
             axd[ax_name].set(yticklabels='')
             
         plt.show()
-        #breakpoint()
-        # hist_crossings, bin_edges = np.histogram(crossings_dawn_query.loc[:, ['rho']], 
-        #                                          density=True, 
-        #                                          bins=rho_bin_edges)
-        # # hist_Juno, _ = np.histogram(rpl_Juno[0, indx_Juno],
-        # #                          density=True, 
-        # #                          bins=np.arange(30, 140, 10))
-        
-        # # hist_joint = hist_crossings/hist_Juno
-        
-        
-        
-        # fig, axs = plt.subplots(nrows=2, sharex=True)
-        # axs[0].stairs(hist_crossings, bin_edges, linewidth=4, label='Crossings')
-        # #axs[0].stairs(hist_Juno, bin_edges, linewidth=4, label='Juno')
-        # axs[0].legend()
-        
-        # #axs[1].stairs(hist_joint, bin_edges)
-        
-        # plt.show()
-        
-        
-        
-        # # res = np.histogram2d(rpl_crossings[0, indx_crossings], sw_mme_sample['p_dyn'],
-        # #                      [np.arange(30, 140, 10), np.arange(0.01, 0.11, 0.01)])
-        
-        # # plt.imshow(res[0], origin='lower', extent=[30, 130, 0.01, 0.1], aspect=1000)
-        # # plt.show()
-        
-        # plt.scatter(rpl_crossings[0, indx_crossings], sw_mme_sample['p_dyn'])
-        # plt.show()
     
     breakpoint()
