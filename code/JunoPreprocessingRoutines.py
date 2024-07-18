0d3d60d8c5b1206f09ca89fa2a938622e58de678
mjrutala <mrutala@bu.edu>
2024-07-18 13:45:04 -0400

    Modified make_CombinedCrossingsList() to allow you to choose which lists you wish to sample; added convert_DirectionToLocation() which translates in/out tags in the crossings lists to a time-dependent location of the spacecraft

--- code/JunoPreprocessingRoutines.py
+++ code/JunoPreprocessingRoutines.py
@@ -28,26 +28,35 @@ def get_paths():
     
     return paths_dict
     
-def make_CombinedCrossingsList(boundary = 'BS'):
+def make_CombinedCrossingsList(boundary = 'BS', which = ['Louis', 'Kurth', 'Ebert']):
     import spiceypy as spice
     
     match boundary.lower():
         case ('bs' | 'bowshock' | 'bow shock'):
-            crossing_data_Louis2023 = read_Louis2023_CrossingList(bs=True)
+            crossing_data = []
             
-            crossing_data_Kurth = read_Kurth_CrossingList(bs=True)
-            #   Dropping items which didn't have a clearly listed crossing direction
-            crossing_data_Kurth = crossing_data_Kurth[crossing_data_Kurth['direction'].notna()]
+            if 'Louis' in which: 
+                crossing_data.append(read_Louis2023_CrossingList(bs=True))
             
-            crossing_data_Ebert = read_Ebert_CrossingList(bs=True)
+            if 'Kurth' in which:
+                crossing_data_Kurth = read_Kurth_CrossingList(bs=True)
+                #   Dropping items which didn't have a clearly listed crossing direction
+                crossing_data.append(crossing_data_Kurth[crossing_data_Kurth['direction'].notna()])
             
-            crossings_df = pd.concat([crossing_data_Louis2023, crossing_data_Kurth, crossing_data_Ebert], axis=0, join="outer")
+            if 'Ebert' in which:
+                crossing_data.append(read_Ebert_CrossingList(bs=True))
+            
+            crossings_df = pd.concat(crossing_data, axis=0, join="outer")
         case ('mp' | 'magnetopause'):
-            crossing_data_Louis2023 = read_Louis2023_CrossingList(mp=True)
+            crossing_data = []
             
-            crossing_data_Ebert = read_Ebert_CrossingList(mp=True)
+            if 'Louis' in which:
+                crossing_data.append(read_Louis2023_CrossingList(mp=True))
             
-            crossings_df = pd.concat([crossing_data_Louis2023, crossing_data_Ebert], axis=0, join="outer")
+            if 'Ebert' in which:
+                crossing_data.append(read_Ebert_CrossingList(mp=True))
+            
+            crossings_df = pd.concat(crossing_data, axis=0, join="outer")
         case _:
             crossings_df = None
     
@@ -129,6 +138,134 @@ def make_HourlyCrossingList(df):
     
     return df
 
+def convert_DirectionToLocation(starttime, stoptime, bs_crossings, mp_crossings, resolution=1):
+    import spiceypy as spice
+    import numpy as np
+    import tqdm
+    
+    # if bs_df['direction'].isnull().values.any():
+    #     print('All crossing directions are needed to analyze the spacecraft location!')
+    #     breakpoint()
+    
+    datetimes = np.arange(starttime, stoptime, dt.timedelta(minutes=resolution), dtype=dt.datetime)
+    
+    with spice.KernelPool([get_paths()['PlanetaryMetakernel'].as_posix(), get_paths()['JunoMetakernel'].as_posix()]):
+        
+        R_J = spice.bodvcd(599, 'RADII', 3)[1][1]
+        
+        ets = spice.datetime2et(datetimes)
+        
+        pos, lt = spice.spkpos('Juno', ets, 'Juno_JSS', 'None', 'Jupiter')
+        pos = pos.T / R_J
+    
+    df = pd.DataFrame({'x': pos[0], 'y': pos[1], 'z':pos[2]}, 
+                      index=datetimes)
+    
+    #   Subset the bow shock df and magnetopause df such that they lie between starttime and stoptime
+    
+    #   Loop over entries, starting with the bow shock
+    df['in_sw'] = np.nan
+    df['in_msh'] = np.nan
+    df['in_msp'] = np.nan
+    
+    # last_indx = pd.Timestamp(starttime)
+    # #   These intervals should be [last_indx, indx)
+    # for indx, row in bs_crossings.iterrows():
+        
+    #     #   If the spacecraft just crossed *in* to the bow shock, it had been in the solar wind since last_indx
+    #     if row['direction'].lower() == 'in':
+    #         df.loc[last_indx:indx, 'in_sw'] = 1
+    #         df.loc[last_indx:indx, 'in_msh'] = 0
+    #         df.loc[last_indx:indx, 'in_msp'] = 0
+        
+    #     #   If the spacecraft just crossed *out* of the bow shock, it had been in the magnetosheath or magnetosphere since last_indx
+    #     if row['direction'].lower() == 'out':
+    #         df.loc[last_indx:indx, 'in_sw'] = 0
+    #         df.loc[last_indx:indx, 'in_msh'] = 1
+    #         df.loc[last_indx:indx, 'in_msp'] = 1
+        
+    #     last_indx = indx
+    
+    
+    # last_indx = pd.Timestamp(starttime)
+    # #   These intervals should be [last_indx, indx)
+    # for indx, row in mp_crossings.iterrows():
+        
+    #     #   If the spacecraft just crossed *in* to the magnetopause, it had been in the magnetosheath alone
+    #     if row['direction'].lower() == 'in':
+    #         #df.loc[last_indx:indx, 'in_msh'] = 1
+    #         df.loc[last_indx:indx, 'in_msp'] = 0
+        
+    #     #   If the spacecraft just crossed *out* of the magnetopause, it had been in the magnetosphere alone
+    #     if row['direction'].lower() == 'out':
+    #         df.loc[last_indx:indx, 'in_msh'] = 0
+    #         #df.loc[last_indx:indx, 'in_msp'] = 1
+            
+    #     last_indx = indx
+    
+    last_indx = pd.Timestamp(starttime)
+    combined_crossings = pd.concat([bs_crossings, mp_crossings], axis='index')
+    interleaved_crossings = combined_crossings.sort_index()
+    
+    for indx, row in interleaved_crossings.iterrows():
+        if row['boundary'] == 'bow shock':
+            if row['direction'] == 'in':
+                df.loc[last_indx:indx, 'in_sw'].iloc[:-1] = 1
+                df.loc[last_indx:indx, 'in_msh'].iloc[:-1] = 0
+                df.loc[last_indx:indx, 'in_msp'].iloc[:-1] = 0
+            if row['direction'] == 'out':
+                df.loc[last_indx:indx, 'in_sw'].iloc[:-1] = 0
+                df.loc[last_indx:indx, 'in_msh'].iloc[:-1] = 1
+                df.loc[last_indx:indx, 'in_msp'].iloc[:-1] = 0
+        if row['boundary'] == 'magnetopause':
+            if row['direction'] == 'in':
+                df.loc[last_indx:indx, 'in_sw'].iloc[:-1] = 0
+                df.loc[last_indx:indx, 'in_msh'].iloc[:-1] = 1
+                df.loc[last_indx:indx, 'in_msp'].iloc[:-1] = 0
+            if row['direction'] == 'out':
+                df.loc[last_indx:indx, 'in_sw'].iloc[:-1] = 0
+                df.loc[last_indx:indx, 'in_msh'].iloc[:-1] = 0
+                df.loc[last_indx:indx, 'in_msp'].iloc[:-1] = 1
+                
+        last_indx = indx
+        
+    df['location'] = np.nan
+    df.loc[df['in_sw'] == 1, 'location'] = 1
+    df.loc[df['in_msh'] == 1, 'location'] = 2
+    df.loc[df['in_msp'] == 1, 'location'] = 3
+    
+    import matplotlib.pyplot as plt
+    fig, axs = plt.subplots(nrows=3, sharex=True)
+    axs[0].plot(df['in_sw'], color='C0')
+    axs[1].plot(df['in_msh'], color='C1')
+    axs[2].plot(df['in_msp'], color='C2')
+    plt.show()
+    
+    fig, axd = plt.subplot_mosaic([['XY', 'ZY'],
+                                   ['XZ', '.']])
+    
+    axd['XY'].scatter(df['x'], df['y'],
+                      c = df['location'], s = 2)
+    
+    axd['XZ'].scatter(df['x'], df['z'],
+                      c = df['location'], s = 2)
+    
+    axd['ZY'].scatter(df['z'], df['y'],
+                      c = df['location'], s = 2)
+    
+    
+    axd['XY'].set(xlim = (150, -150),
+                  ylim = (-150, 150))
+    axd['XZ'].set(xlim = (150, -150),
+                  ylim = (-150, 150))
+    axd['ZY'].set(xlim = (-150, 150),
+                  ylim = (-150, 150))
+    
+            
+    breakpoint()
+        
+    
+
 def get_HourlyFromDatetimes(ts):
     #   Get the nearest hour to hourly precision
     res = [t.replace(minute=0, second=0, microsecond=0) + 
@@ -191,12 +328,13 @@ def read_Ebert_CrossingList(mp=False, bs=True):
     
     return crossings
 
-def plot_CrossingsAndTrajectories_XYPlane(joy=False):
+def plot_CrossingsAndTrajectories_XYPlane(joy=False, mme=False):
     import spiceypy as spice
     import numpy as np
     
     import matplotlib as mpl
     import matplotlib.pyplot as plt
+    import matplotlib as mpl
     
     import JoyBoundaryCoords as JBC
 
@@ -233,13 +371,14 @@ def plot_CrossingsAndTrajectories_XYPlane(joy=False):
     
     spice.kclear()
     
-    # sw_models = MMESH_reader.fromFile('/Users/mrutala/projects/JupiterBoundaries/mmesh_run/MMESH_atJupiter_20160301-20240301_withConstituentModels.csv')
-    # sw_mme = sw_models.xs('ensemble', axis='columns', level=0)
+    sw_models = MMESH_reader.fromFile('/Users/mrutala/projects/JupiterBoundaries/mmesh_run/MMESH_atJupiter_20160301-20240301_withConstituentModels.csv')
+    sw_mme = sw_models.xs('ensemble', axis='columns', level=0)
     
     with plt.style.context('/Users/mrutala/code/python/mjr_presentation.mplstyle'):
         fig, axs = plt.subplots(ncols=2, sharey=True, sharex=True, 
                                 figsize=(8,6))
-        plt.subplots_adjust(left=0.125, bottom=0.15, right=0.975, top=0.7,
+        bottom, left, top, right = 0.275, 0.125, 0.825, 0.975
+        plt.subplots_adjust(bottom=bottom, left=left, top=top, right=right,
                             wspace=0.075)
         
         
@@ -265,9 +404,28 @@ def plot_CrossingsAndTrajectories_XYPlane(joy=False):
                     color='xkcd:light gray', linewidth=0.5, 
                     zorder=1)
             
-            ax.scatter(*pos_df.loc[crossing_data.index, ['y', 'x']].to_numpy().T,
-                       color = 'white', s = 6, 
-                       marker = marker, zorder=2)
+            #   Scatter plot for crossings
+            if mme:
+                if crossing_type == 'bs':
+                    crossing_data = bs_crossing_data
+                    marker = 'x'
+                if crossing_type == 'mp':
+                    crossing_data = mp_crossing_data
+                    marker = 'o'
+                    
+                bounds = np.logspace(-2, -1, 11)
+                cmap = plt.get_cmap('magma', 11)
+                norm = mpl.colors.BoundaryNorm(bounds, cmap.N)
+                
+                ax.scatter(*pos_df.loc[crossing_data.index, ['y', 'x']].to_numpy().T,
+                           c = sw_mme.loc[crossing_data.index, 'p_dyn'],
+                           cmap = cmap, norm = norm, s = 12, 
+                           marker = marker, zorder=2)
+            else:
+            
+                ax.scatter(*pos_df.loc[crossing_data.index, ['y', 'x']].to_numpy().T,
+                           color = 'white', s = 12, 
+                           marker = marker, zorder=2)
             
             #   Plot years for reference
             for year in [2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024]:
@@ -277,43 +435,51 @@ def plot_CrossingsAndTrajectories_XYPlane(joy=False):
                 indx = ((pos_df.index > dt.datetime(year, 1, 1) - dt.timedelta(days = 40)) &
                         (pos_df.index < dt.datetime(year, 1, 1) + dt.timedelta(days = 40)))
                 
-                max_r = np.max(pos_df.iloc[indx].loc[:, 'r']) + 20
+                max_r = np.max(pos_df.iloc[indx].loc[:, 'r']) + 15
                 
                 ax.annotate(year, (max_r*np.cos(ang), max_r*np.sin(ang)), (0,0), 
                             xycoords = 'data', textcoords = 'offset fontsize', 
                             rotation = (-90 - (ang * 180/np.pi + 360)),
                             ha = 'center', va = 'center', annotation_clip=True,
-                            fontsize=12)
+                            fontsize='xx-small')
                 
             #   Plot Joy bounds (optional)
             if joy:
                 x_joy = JBC.find_JoyBoundaries(0.02, boundary = crossing_type.upper(), y=y_joy, z=0)
-                ax.plot(y_joy, x_joy[1], label = r'$p_{{dyn}} = {:.2f}$ nPa ($16^{{th}}$ %ile)'.format(0.03),
+                ax.plot(y_joy, x_joy[1], label = r'$p_{{dyn}} = {:.2f} = 10^{{ {:.1f} }}$ nPa ($16^{{th}}$ %ile)'.format(0.03, np.log10(0.03)),
                          color='C2', linewidth=1.5, linestyle=linestyle_joy)
                 
                 x_joy = JBC.find_JoyBoundaries(0.05, boundary = crossing_type.upper(), y=y_joy, z=0)
-                ax.plot(y_joy, x_joy[1], label = r'$p_{{dyn}} = {:.2f}$ nPa ($50^{{th}}$ %ile)'.format(0.08),
+                ax.plot(y_joy, x_joy[1], label = r'$p_{{dyn}} = {:.2f} = 10^{{ {:.1f} }}$ nPa ($50^{{th}}$ %ile)'.format(0.08, np.log10(0.08)),
                          color='C4', linewidth=1.5, linestyle=linestyle_joy)
                 
                 x_joy = JBC.find_JoyBoundaries(0.13, boundary = crossing_type.upper(), y=y_joy, z=0)
-                ax.plot(y_joy, x_joy[1], label = r'$p_{{dyn}} = {:.2f}$ nPa ($84^{{th}}$ %ile)'.format(0.13),
+                ax.plot(y_joy, x_joy[1], label = r'$p_{{dyn}} = {:.2f} = 10^{{ {:.1f} }}$ nPa ($84^{{th}}$ %ile)'.format(0.13, np.log10(0.13)),
                          color='C5', linewidth=1.5, linestyle=linestyle_joy)
                 
                 x_joy = JBC.find_JoyBoundaries(0.47, boundary = crossing_type.upper(), y=y_joy, z=0)
-                ax.plot(y_joy, x_joy[1], label = r'$p_{{dyn}} = {:.2f}$ nPa ($99^{{th}}$ %ile)'.format(0.47),
+                ax.plot(y_joy, x_joy[1], label = r'$p_{{dyn}} = {:.2f} = 10^{{ {:.1f} }}$ nPa ($99^{{th}}$ %ile)'.format(0.47, np.log10(0.47)),
                          color='C0', linewidth=1.5, linestyle=linestyle_joy)
                 
                 if crossing_type == 'bs': 
-                    ax.legend(loc='lower center', bbox_to_anchor=(1.05, 1.2), ncol=2)
+                    ax.legend(loc='lower center', bbox_to_anchor=(1.04, 1.075), ncol=2, fontsize='small')
                 
             ax.set(xlim = [-150, 150], ylim = [150,-150],
                    aspect = 1)
             
-        axs[0].set(title='Bow Shock Crossings \n (n = 117)')
-        axs[1].set(title='Magnetopause Crossings \n (n = 454)')
+        axs[0].set_title('Bow Shock Crossings (n = 117)', fontsize='small')
+        axs[1].set_title('Magnetopause Crossings (n = 454)', fontsize='small')
         
-        fig.supxlabel(r'$Y_{JSS}$ [$R_J$] (+ duskward)', x=0.55)
+        fig.supxlabel(r'$Y_{JSS}$ [$R_J$] (+ duskward)', x=0.55, y=0.175)
         axs[0].set(ylabel = r'$X_{JSS}$ [$R_J$] (+ sunward)')
+        
+        if mme:
+            ax2 = fig.add_axes([left, 0.125, right-left, 0.05])
+            
+            cb = plt.colorbar(mpl.cm.ScalarMappable(norm=norm, cmap=cmap), cax=ax2, orientation='horizontal')
+            #cb.ax.set_yticklabels([r'$10^{{{:.1f}}}$'.format(np.log10(num)) for num in cb.get_ticks()])
+            cb.ax.set_xticklabels([r'{:.1f}'.format(np.log10(num)) for num in bounds])
+            ax2.set(xlabel=r'$p_{dyn}$ [log(nPa)]')
         
         plt.show()
 
