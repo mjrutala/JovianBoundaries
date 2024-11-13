8d92ccdcb423af55442b749befa08bddf53e6845
mjrutala <mrutala@bu.edu>
2024-11-13 09:30:56 +0000

    Wrote a wrapper around _HybridMCMC()->[the actual sampler] to handle arguments, pickling, plotting, and saving; implemented bimodal fits in _HybridMCMC()

--- code/HybridMCMC.py
+++ code/HybridMCMC.py
@@ -17,6 +17,8 @@ import pandas as pd
 import datetime as dt
 import corner
 import pickle
+import pprint
+import os
 
 from pytensor.graph import Apply, Op
 import xarray
@@ -37,20 +39,185 @@ try:
 except:
     pass
 
-resolution = '10Min'
-spacecraft_to_use = ['Ulysses', 'Galileo', 'Cassini', 'Juno']
-# boundary = 'BS'
-# fraction = 0.25
-# n_pressures = 1
-# n_draws = 500
-# n_chains = 3
-
-# model_name = 'Shuelike'
-
-def run_HybridMCMC():
+def HybridMCMC(boundary, model_name, 
+               fraction,
+               n_pressures, n_draws, n_chains, n_modes):
     "Shell to run HybridMCMC and save results"
+    
+    # Get the run start time for file-saving and make a corresponding dir
+    now_str = dt.datetime.now().strftime("%Y%m%d%H%M%S")
+    savedir = '/Users/mrutala/projects/JupiterBoundaries/posteriors/{}/'.format(now_str)
+    if not os.path.exists(savedir):
+        os.makedirs(savedir)
+    else:
+        msg = "The path '{}' already exists-- and shouldn't! Returning..."
+        print(msg.format(savedir))
+        return
+    
+    # Dictionary of arguments to be passed
+    args = {'boundary': boundary,                                               # Boundary param
+            'model_name': model_name,                                           # Boundary param
+            'spacecraft_to_use': ['Ulysses', 'Galileo', 'Cassini', 'Juno'],     # Data param
+            'resolution': '10Min',                                              # Data param
+            'fraction': fraction,                                               # Data param
+            'n_pressures': n_pressures,                                         # MCMC param
+            'n_draws': n_draws,                                                 # MCMC param
+            'n_chains': n_chains,                                               # MCMC param 
+            'n_modes': n_modes}                                                 # MCMC param 
+    
+    # Run the MCMC sampler
+    posterior, model_dict = _HybridMCMC(**args)
+    
+    # Write to file
+    stem = "{model_name}-{boundary}_".format(**args) + now_str
+    
+    # Inputs
+    i_filepath = savedir + stem + "_input.txt"
+    with open(i_filepath, 'w') as f:
+        pprint.pprint(args, f)
+        
+    # Posterior
+    p_filepath = savedir + stem + "_posterior.pkl"
+    with open(p_filepath, 'wb') as f:
+        pickle.dump(posterior, f)
+    
+    #   Plot a trace to check for convergence
+    fig, axs = plt.subplots(nrows=len(posterior), ncols=2, figsize=(6.5, 2*len(posterior)))
+    plt.subplots_adjust(bottom=0.025, left=0.05, top=0.975, right=0.95,
+                       hspace=0.5, wspace=0.2)
+    az.plot_trace(posterior, axes=axs, rug=True)
+    plt.savefig(savedir + stem + "_traceplot.png", dpi=300)
+    
+    #   Plot a corner plot to investigate covariance
+    figure = corner.corner(posterior, 
+                           quantiles=[0.16, 0.5, 0.84],
+                           show_titles=True,)
+    figure.set_size_inches(len(posterior),len(posterior))
+    plt.savefig(savedir + stem + "_cornerplot.png", dpi=300)
+    
+    plt.show()
 
-def HybridMCMC(boundary, spacecraft_to_use, resolution,
+    return posterior, model_dict
+
+def _HybridMCMC(boundary, model_name,
+                spacecraft_to_use, resolution, fraction, 
+                n_pressures, n_draws, n_chains, n_modes):
+    
+    positions_df = postproc.PostprocessCrossings(boundary, spacecraft_to_use = spacecraft_to_use)
+    
+    positions_df = positions_df.query('r_upperbound < 1e3 & r_lowerbound < 1e3')
+    
+    # FOR TESTING, TO MAKE THE MCMC SAMPLER RUN FASTER
+    positions_df = positions_df.sample(frac=fraction, axis='rows', replace=False)
+    n = len(positions_df)
+    
+    from scipy.stats import skewnorm
+    
+    # For readability, pull out indiviudal variables to be passed to PyMC
+    r = positions_df['r'].to_numpy('float64')
+    t = positions_df['t'].to_numpy('float64')
+    p = positions_df['p'].to_numpy('float64')
+    
+    r_lower = positions_df['r_lowerbound'].to_numpy('float64')
+    r_upper = positions_df['r_upperbound'].to_numpy('float64')
+    
+    p_dyn_loc   = positions_df['p_dyn_loc'].to_numpy('float64')
+    p_dyn_scale = positions_df['p_dyn_scale'].to_numpy('float64')
+    p_dyn_alpha = positions_df['p_dyn_a'].to_numpy('float64')
+    p_dyn_mu    = skewnorm.mean(loc = p_dyn_loc, scale = p_dyn_scale, a = p_dyn_alpha)
+    p_dyn_sigma = skewnorm.std(loc = p_dyn_loc, scale = p_dyn_scale, a = p_dyn_alpha)
+
+    test_pressure_dist = pm.SkewNormal.dist(mu = p_dyn_mu, sigma = p_dyn_sigma, alpha = p_dyn_alpha)
+    test_pressure_draws = pm.draw(test_pressure_dist, draws=n_pressures)
+    # Hacky, but assume negative pressures correspond to small pressures
+    test_pressure_draws[test_pressure_draws < 0] = np.min(test_pressure_draws[test_pressure_draws > 0])
+    if len(np.shape(test_pressure_draws)) < 2:
+        test_pressure_draws = [test_pressure_draws]
+    
+    # Model and MCMC params
+    posterior_list = []    
+    model_dict = BM.init(model_name)
+    
+    print("Testing {} pressure draws".format(len(test_pressure_draws)))
+    for pressure_draw in test_pressure_draws:
+        
+        with pm.Model() as test_potential:
+    
+            p_dyn_obs = pressure_draw
+            
+            #   N.B.: 'param_dict' must keep the same name, as it is used in code string evaluation below
+            param_dict = {}
+            for param_name in model_dict['param_distributions'].keys():
+                param_dist = model_dict['param_distributions'][param_name]
+                
+                param = param_dist(param_name, **model_dict['param_descriptions'][param_name])
+                
+                param_dict[param_name] = param
+            
+            # Assign variable names to a list of tracking in the sampler
+            param_names = list(param_dict.keys())
+            tracked_var_names = []
+            for pname in param_names:
+                tracked_var_names.append(pname)
+            
+            params = list(param_dict.values())
+            
+            # The predicted mean location of the boundary is a function of pressure
+            mu0 = pm.Deterministic("mu0", model_dict['model'](params, [t, p, p_dyn_obs]))
+            
+            # This section adds a penalty for creating a closed boundary (i.e., no open field lines)
+            if 'Shue' in model_name:
+                flare_pred =  pm.Deterministic("flare_pred", model_dict['model'](params, [t, p, p_dyn_obs], return_a_f=True))
+                closed_penalty = pm.Potential("closed_penalty", pm.math.switch(flare_pred < 0.5, -np.inf, 0))
+            
+            # The observed mean location will vary based on internal drivers
+            # In a spherical coordinate system, the variation in r must increase with theta
+            r_obs = pm.Uniform("r_obs", lower = r_lower, upper = r_upper)
+            
+            # Make sigma increase with t
+            sigma_b = pm.HalfNormal("sigma_b", 1.0)
+            sigma_m = pm.HalfNormal("sigma_m", 1.0)
+            sigma_fn = pm.Deterministic("sigma_fn", sigma_b + sigma_m * mu0)
+            tracked_var_names.extend(["sigma_b", "sigma_m"])
+
+            # likelihood = pm.Potential("likelihood", pm.logp(pm.Normal.dist(mu = mu_pred, sigma = sigma_fn), value=r_obs))
+            # sigma_dist = pm.Normal("sigma_dist", 0, 1)
+            # likelihood = pm.Potential("likelihood", pm.logp(pm.Normal.dist(mu = mu_pred + (sigma_dist * sigma_fn)), value=r_obs))
+            
+            # Optionally, handle multiple modes           
+            if n_modes == 1:
+                likelihood = pm.Normal("likelihood", mu = mu0 - r_obs, sigma = sigma_fn, observed = np.zeros(n))
+                
+            elif n_modes == 2:
+                buffer1 = pm.HalfNormal("buffer1", sigma=5)
+                tracked_var_names.append("buffer1")
+                mu1 = pm.Deterministic("mu1", mu0 - buffer1)
+                
+                # Weights for multi-modal fits
+                w = pm.Dirichlet("w", np.ones(2))
+                
+                dists = [pm.Normal.dist(mu = mu0, sigma = sigma_fn),
+                         pm.Normal.dist(mu = mu1, sigma = sigma_fn)]
+                # likelihood = pm.Mixture("likelihood", w=w, comp_dists = dists,
+                #                         observed = np.zeros(n))
+                x = pm.Mixture.dist(w=w, comp_dists = dists)
+                likelihood = pm.Potential("likelihood", pm.logp(x, value = r_obs))
+            
+        with test_potential:
+            idata = pm.sample(tune=512, draws=n_draws, chains=n_chains, cores=3,
+                              var_names = tracked_var_names,
+                              target_accept=0.90)
+                              # init = 'adapt_diag+jitter') # prevents 'jitter', which might move points init vals around too much here
+        
+        posterior = idata.posterior
+        posterior_list.append(posterior)
+    
+    posterior = xarray.concat(posterior_list, dim = 'chain')
+        
+    return posterior, model_dict
+
+
+def _HybridMCMC_outdated(boundary, spacecraft_to_use, resolution,
                model_name,
                fraction, n_pressures, n_draws, n_chains):
     
@@ -189,15 +356,16 @@ def HybridMCMC(boundary, spacecraft_to_use, resolution,
             
             # var = pm.Normal("var", 0, 1)
             
-            likelihood = pm.Potential("likelihood", pm.logp(pm.Normal.dist(mu = mu_pred, sigma = sigma_fn), value=r_obs))
+            # likelihood = pm.Potential("likelihood", pm.logp(pm.Normal.dist(mu = mu_pred, sigma = sigma_fn), value=r_obs))
+            likelihood = pm.Normal("likelihood", mu = mu_pred - r_obs, sigma = sigma_fn, observed = np.zeros(len(r)))
             
             # sigma_dist = pm.Normal("sigma_dist", 0, 1)
             # likelihood = pm.Potential("likelihood", pm.logp(pm.Normal.dist(mu = mu_pred + (sigma_dist * sigma_fn)), value=r_obs))
     
         with test_potential:
-            idata = pm.sample(tune=1000, draws=n_draws, chains=n_chains, cores=3,
+            idata = pm.sample(tune=512, draws=n_draws, chains=n_chains, cores=3,
                               var_names = tracked_var_names,
-                              target_accept=0.95)
+                              target_accept=0.80)
                               # init = 'adapt_diag+jitter') # prevents 'jitter', which might move points init vals around too much here
         
         posterior = idata.posterior
@@ -226,3 +394,5 @@ def HybridMCMC(boundary, spacecraft_to_use, resolution,
     posterior_filepath = '/Users/mrutala/projects/JupiterBoundaries/posteriors/' + posterior_filename + '.pkl'
     with open(posterior_filepath, 'wb') as f:
         pickle.dump(posterior, f)
+        
+    return posterior, model_dict
